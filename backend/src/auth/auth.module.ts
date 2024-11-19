416333989c849f878ce6996d7c249439d58da9d8
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2024-11-19 03:28:04 +0100

    Finish auth module

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from '../users/users.module';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { RolesGuard } from './guards';
+import { User } from '../users/entities/user.entity';
 
 @Module({
   imports: [
@@ -21,6 +23,7 @@ import { RolesGuard } from './guards';
         },
       }),
     }),
+    TypeOrmModule.forFeature([User]),
   ],
   providers: [AuthService, JwtStrategy, RolesGuard],
   controllers: [AuthController],
