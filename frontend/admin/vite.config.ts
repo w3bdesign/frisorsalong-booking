f9fbce53bcb48a8e52e7593910a052db640c77c1
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2024-11-19 04:24:19 +0100

    Typescript setup

--- frontend/admin/vite.config.ts
+++ frontend/admin/vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'node:url';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  test: {
+    environment: 'jsdom',
+    globals: true
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    port: 3001,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true
+      }
+    }
+  }
+});
