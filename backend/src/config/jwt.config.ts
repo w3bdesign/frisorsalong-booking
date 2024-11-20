93639a7e194a82b2af0990bc86015113e474b913
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2024-11-20 07:02:27 +0100

    Fix test

--- backend/src/config/jwt.config.ts
+++ backend/src/config/jwt.config.ts
@@ -1,8 +1,8 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('jwt', () => ({
-  secret: process.env.JWT_SECRET || 'your-secret-key',
+  secret: process.env.JWT_SECRET,
   signOptions: {
-    expiresIn: process.env.JWT_EXPIRES_IN || '24h',
+    expiresIn: process.env.JWT_EXPIRATION || '1h',
   },
 }));
