a58e4f6b535c40da141b00b91faab5c46259cf8a
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2026-02-21 01:27:00 +0100

    Prefre Number.parseInt

--- backend/src/config/cache.config.ts
+++ backend/src/config/cache.config.ts
@@ -2,6 +2,6 @@ import { registerAs } from "@nestjs/config";
 
 export default registerAs("cache", () => ({
   host: process.env.REDIS_HOST || "localhost",
-  port: parseInt(process.env.REDIS_PORT, 10) || 6379,
-  ttl: parseInt(process.env.CACHE_TTL, 10) || 5 * 60, // 5 minutes
+  port: Number.parseInt(process.env.REDIS_PORT, 10) || 6379,
+  ttl: Number.parseInt(process.env.CACHE_TTL, 10) || 5 * 60, // 5 minutes
 }));
